3f3f11f0a7aa1dcf2650005b4f58bc884c9525f1
unknown <atesz_a_nyul@hotmail.com>
2014-11-28 13:34:40 +0100

    Most kezdodik a layout magic

--- src/com/newl/calendar/TaskView.java
+++ src/com/newl/calendar/TaskView.java
@@ -3,9 +3,14 @@ package com.newl.calendar;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
 
 public class TaskView extends JFrame {
 
@@ -22,10 +27,35 @@ public class TaskView extends JFrame {
 		
 		JTable table = new JTable(data);
 		add(BorderLayout.CENTER, table);
+		
 		JScrollPane scrollPane = new JScrollPane(table);
 		add(scrollPane);
+		
 		table.setFillsViewportHeight(true);
         table.setVisible(true);
+        
+        JPanel inputPanel = new JPanel();
+        add(inputPanel, BorderLayout.EAST);
+        inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS));
+        
+        JTextField titleField = new JTextField(20);
+        JTextField notesField = new JTextField(20);
+        inputPanel.add(new JLabel("Title:"));
+        inputPanel.add(titleField);
+        inputPanel.add(new JLabel("Notes:"));
+        inputPanel.add(notesField);
+        
+        JComboBox<Integer> dateY = new JComboBox();
+        JComboBox<String> dateM = new JComboBox();
+        JComboBox<Integer> dateD = new JComboBox();
+        inputPanel.add(dateY);
+        inputPanel.add(dateM);
+        inputPanel.add(dateD);
+        
+        JComboBox<Integer> remindY = new JComboBox();
+        JComboBox<String> remindM = new JComboBox();
+        JComboBox<Integer> remindD = new JComboBox();
+        
         
         System.out.println("initComponents Done!");
 	}
